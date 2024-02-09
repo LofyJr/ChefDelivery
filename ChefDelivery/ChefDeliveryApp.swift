958aeb2cb63f607d60a65b76f250d459a78a28a5
Juliano Lofy Junior <juliano.junior@havan.com.br>
2024-02-09 10:21:48 -0300

    Initial Commit

---
+++ ChefDelivery/ChefDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryApp.swift
+//  ChefDelivery
+//
+//  Created by Juliano Lofy Junior on 09/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ChefDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
