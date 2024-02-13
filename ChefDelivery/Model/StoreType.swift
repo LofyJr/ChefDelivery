8772739207f35ac3cd0692720f1d0e13cce4b7af
Juliano Lofy Junior <juliano.junior@havan.com.br>
2024-02-13 14:56:24 -0300

    feature: requisition http

--- ChefDelivery/Model/StoreType.swift
+++ ChefDelivery/Model/StoreType.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct StoreType: Identifiable {
+struct StoreType: Identifiable, Decodable {
     let id: Int
     let name: String
     let logoImage: String
@@ -15,4 +15,14 @@ struct StoreType: Identifiable {
     let location: String
     let stars: Int
     let products: [ProductType]
+
+    private enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case name = "name"
+        case logoImage = "logo_image"
+        case headerImage = "header_image"
+        case location = "location"
+        case stars = "stars"
+        case products = "products"
+    }
 }
