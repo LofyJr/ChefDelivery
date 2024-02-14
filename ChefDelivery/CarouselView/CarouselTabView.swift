dc9be213f806bcb98cb73c47c3393304d740542c
Juliano Lofy Junior <juliano.junior@havan.com.br>
2024-02-14 11:32:49 -0300

    feature: create home viwe

--- ChefDelivery/CarouselView/CarouselTabView.swift
+++ ChefDelivery/CarouselView/CarouselTabView.swift
@@ -8,19 +8,35 @@
 import SwiftUI
 
 struct CarouselTabView: View {
+
+    @State private var currentIndex = 1
+
+
     let ordersMock: [OrderType] = [
         OrderType(id: 1, name: "banner burguer", image: "barbecue-banner"),
         OrderType(id: 2, name: "banner prato feito", image: "brazilian-meal-banner"),
         OrderType(id: 3, name: "banner pokes", image: "pokes-banner")
     ]
     var body: some View {
-        TabView {
+        TabView(selection: $currentIndex){
             ForEach(ordersMock) { mock in
                 CarouselItemView(orderType: mock)
+                    .tag(mock.id)
             }
         }
         .frame(height: 180)
         .tabViewStyle(.page(indexDisplayMode: .always))
+        .onAppear {
+            Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { _ in
+                withAnimation(.easeIn(duration: 1)) {
+                    if currentIndex > ordersMock.count {
+                        currentIndex = 1
+                    } else {
+                        currentIndex += 1
+                    }
+                }
+            }
+        }
     }
 }
 
